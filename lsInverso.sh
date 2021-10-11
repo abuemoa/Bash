c8668dfbdcfc0db3d5aa11c99c703a0ed62c64e5
Alejandro <88618807+abuemoa@users.noreply.github.com>
2021-10-11 11:02:34 +0200

    Create lsInverso.sh

---
+++ lsInverso.sh
@@ -0,0 +1,13 @@
+# Recibir varios nombres de archivo como parámetros, y para cada uno validar si el nombre corresponde a un archivo común existente, y si es así mostrarlo en pantalla paginando.
+
+#!/bin/bash
+
+archivos=($(echo $*))
+echo "Los siguientes archivos están presentes: "
+for i in "${archivos[@]}"
+do
+    if [ -f $i ]; then
+	echo "el archivo "$i" existe"  
+    fi
+done | less
+ 
