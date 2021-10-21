53c1b6e457ab0a3a3957bd708e93f2d6d6d65b28
Alejandro <88618807+abuemoa@users.noreply.github.com>
2021-10-21 17:13:26 +0200

    Código simplificado
    Aquí el código de manera simplificada

--- noProprietaryCodecs.sh
+++ noProprietaryCodecs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-usuarioFold=$(ls /home)
-
-
+usuarioFold=$(ls -1 /home)
 
 echo "he aquí un listado de los usuarios a los que puede destrozar la vida 
 
@@ -10,21 +8,20 @@ $usuarioFold"
 echo "
 Por favor, introduzca el nombre del usuario al que quiere destrozar la vida"
 read $1 usuario
-counter=$(ls -1 *avi *mp3 2>/dev/null | wc -l)
-size=$(ls -1 /media/alex/*mp3 2>/dev/null | xargs du -cb | awk 'END {print $1}')
-while [[ $usuarioFold -ne $usuario ]]; do
-        read $1 usuario
-        echo "el usuario $usuario no existe, pruebe un usuario de esta lista para destrozar:\n"
-        echo $usuarioFold
-done
+
+
+counter=$(find /home/$usuario \( -name "*mp3" -o -name "*avi" \) | wc -l 2>/dev/null)
+
+size=$(find /home/$usuario \( -name "*mp3" -o -name "*avi" \) -exec du -ch {} + | grep total$)
 
 find /home/$usuario \( -name "*mp3" -o -name "*avi" \) -exec rm -rf {} \; 2>/dev/null
+
 echo "Se han eliminado $counter ficheros"
 echo "Y se han eliminado un total de $size bytes"
 
-if [[ $counter -ne 0 ]]
+if [[ $counter -ne 0 ]] 
 then
         echo "gracias por destrozar la vida al usuario $usuario"       
-else
+else 
         echo "Pues ya si eso a la próxima"
 fi
