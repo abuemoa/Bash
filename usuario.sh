e9bb67ecb4507104aaf6f0c13a895b3404ac3e6e
Alejandro <88618807+abuemoa@users.noreply.github.com>
2021-10-21 22:43:01 +0200

    Whole words
    Ahora encuentra los usuarios solamente si se escribe el nombre completo

--- usuario.sh
+++ usuario.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 read -p "Mari, ponme el usuario que quieras: " usuario
-hora=$(w -h | grep $usuario | awk '{print $4}' | tail -1)
-nombre=$(w -h | grep $usuario | awk '{print $1}')
+hora=$(w -h | grep -w $usuario | awk '{print $4}' | tail -1)
+nombre=$(w -h | grep -w $usuario | awk '{print $1}')
 if [ -z "$nombre" ]; then
-        echo "el usuario no se encuentra conectado"     
+        echo "el usuario no se encuentra conectado"
 else
         echo "El usuario $usuario lleva conectado desde la/s $hora. Digale que apague la Nintendo"
 fi
